f85a0c372f9ad8f6500943210319c9ec20212460
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-13 20:02:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 //your code here
-
- let dragEvents = ["dragover","dragenter", "drop"];
+ let dragEvents = ["dragStart","dragover","dragenter", "drop"];
 
     dragEvents.forEach((eventOfDrag)=>{
-        let areasOfDrop=document.querySelectorAll(".image");
-        for(let i=0; i<areasOfDrop.length; i++){
-			for(let j=0; j<areasOfDrop.length; j++){
-				area.addEventListener(eventOfDrag, (event)=>{
-                event.preventDefault();
-                if(eventOfDrag=="drop"){
+        let areasOfDrop= Array.from(document.querySelectorAll(".image"));
+		let ct=1;
+		areasOfDrop.forEach(div=> {div.id=`div${ct++}`;});
 
-					t=areasOfDrop[j];
-					areasOfDrop[j]=areasOfDrop[i];
-					areasOfDrop[i]=t;
-                   
-                }
+		for (let t of areasOfDrop){
+			t.addEventListener(eventOfDrag, (event)=>{
+				if(eventOfDrag=="dragStart"){
+				event.dataTransfer.setData("div", t.id);
+				event.dataTransfer.setData("text", t.innerText);
+				}
+				
+				if(eventOfDrag=="drop"){
+				let perID= event.dataTransfer.getData("div");
+				let perText=event.dataTransfer.getData("text");
+				let droparea=event.terget;
+					event.dataTransfer.setData("dragid", droparea.id);
+					event.dataTransfer.setData("dragtext", droparea.innerText);
+				droparea.id=perID;
+				droparea.innerText=perText;
 			}
+			})
+			
 		}
             
             })
