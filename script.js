0f17aff4733fc0837ea8c625ed512bf5551ed154
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-13 19:16:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+
+ let dragEvents = ["dragover","dragenter", "drop"];
+
+    dragEvents.forEach((eventOfDrag)=>{
+        let areasOfDrop=document.querySelectorAll(".image");
+        for(let i=0; i<areasOfDrop.length; i++){
+			for(let j=0; j<areasOfDrop.length; j++){
+				area.addEventListener(eventOfDrag, (event)=>{
+                event.preventDefault();
+                if(eventOfDrag=="drop"){
+
+					t=areasOfDrop[j];
+					areasOfDrop[j]=areasOfDrop[i];
+					areasOfDrop[i]=t;
+                   
+                }
+			}
+		}
+            
+            })
