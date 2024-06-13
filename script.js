978ae1312949328edcc36384d96cb081121ca3c1
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-14 00:28:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-//your code here
- let dragEvents = ["dragStart","dragover","dragenter", "drop"];
+let areasOfDrop = Array.from(document.querySelectorAll(".image"));
+let ct = 1;
 
-    dragEvents.forEach((eventOfDrag)=>{
-        let areasOfDrop= Array.from(document.querySelectorAll(".image"));
-		let ct=1;
-		areasOfDrop.forEach(div=> {div.id=`div${ct++}`;});
+areasOfDrop.forEach(div => {
+    div.id = `div${ct++}`;
+});
 
-		for (let t of areasOfDrop){
-			t.addEventListener(eventOfDrag, (event)=>{
-				if(eventOfDrag=="dragStart"){
-				event.dataTransfer.setData("div", t.id);
-				event.dataTransfer.setData("text", t.innerText);
-				}
-				
-				if(eventOfDrag=="drop"){
-				let perID= event.dataTransfer.getData("div");
-				let perText=event.dataTransfer.getData("text");
-				let droparea=event.terget;
-					event.dataTransfer.setData("dragid", droparea.id);
-					event.dataTransfer.setData("dragtext", droparea.innerText);
-				droparea.id=perID;
-				droparea.innerText=perText;
-			}
-			})
-			
-		}
-            
-            })
+areasOfDrop.forEach(div => {
+    div.addEventListener("dragstart", (event) => {
+        event.dataTransfer.setData("div", div.id);
+    });
+
+    div.addEventListener("dragover", (event) => {
+        event.preventDefault();
+    });
+
+    div.addEventListener("drop", (event) => {
+        event.preventDefault();
+        let perID = event.dataTransfer.getData("div");
+        let selectDiv = document.getElementById(perID);
+        let droparea = event.target;
+
+        let temp = selectDiv.style.backgroundImage;
+        selectDiv.style.backgroundImage = droparea.style.backgroundImage;
+        droparea.style.backgroundImage = temp;
+    });
+});
